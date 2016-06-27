9793462d07bcc889add778b1701e3a4c7d16ff85
Sam Walker <samantha.walker-1@colorado.edu>
2016-06-27 10:03:26 -0600

    Update equations.py

--- equations.py
+++ equations.py
@@ -1,1 +1,8 @@
 #### This will contain all the equations that will be be needed to calculate the optimal parameter space.
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from scipy import integrate
+
+
